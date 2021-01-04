3a24b39a85f79857fea4d20b00aa313f5d168f99
cr-trojan23 <pandusrinu1604@gmail.com>
2021-01-05 01:52:57 +0530

    Added Text Extraction

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 import cv2
 from PIL import Image
 from pdf2image import convert_from_path
-import pytesseract
+import pytesseract as pyt
 import sys
 import os
 import numpy
@@ -14,8 +14,9 @@ def toImages(pdf_name):
     for page in pages:
         img_name = "page_" + str(img_count) + ".png"
         page.save(img_name, "PNG")
+        print("Converting " + "Page " + str(img_count) + " into image")
         img_count = img_count + 1
-    print(img_count)
+    print("Found " + str(img_count) + " pages")
 
 def boundPara(img_name):
     image = cv2.imread(img_name)
@@ -24,13 +25,20 @@ def boundPara(img_name):
     bound_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7,7))
     dilate_element = cv2.dilate(threshold, bound_kernel, iterations=6)
     contours = cv2.findContours(dilate_element, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print(len(contours))
     contours = contours[0] if len(contours) == 2 else contours[1]
-    print(len(contours))
+    bounded_img = threshold
     for contour in contours:
         x, y, w, h = cv2.boundingRect(contour)
-        print(x,y,w,h)
-        cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0 ,0), 2)
-    cv2.imwrite("cont.png", image)
-#toImages("/home/kiaria/Documents/318- chp 22.pdf")
-boundPara("/home/kiaria/Documents/github_projects/tescroppy/page_2.png")
+        bounded_img = threshold[y:y+h, x:x+w]
+        custom_conf = "--oem 1 --psm 1 -l eng --tessdata-dir \"/usr/share/tesseract-ocr/4.00/tessdata\""
+        data = pyt.image_to_string(bounded_img, config=custom_conf)
+        file_out = open("Extracted_Test.txt", "a")
+        file_out.write(data)
+        file_out.close
+
+toImages(input("Enter PDF File location \n"))
+
+for i in range(1, img_count):
+    img_name = "page_" + str(i) + ".png"
+    print("Processing ", img_name)
+    boundPara(img_name)
